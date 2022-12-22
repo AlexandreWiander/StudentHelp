c8649c27d25b9109d2b6e4cb2dd4c839e444c40e
Estelle JANS <e.jans@student.helmo.be>
2022-12-22 16:16:44 +0100

    cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,7 +131,7 @@ export default function Home() {
       body: JSON.stringify({ link: mylink})
     });
     var contenu = new TextDecoder("utf-8").decode(await response.arrayBuffer()).toString();
-    console.log(response.url);
+    console.log(response);
     if (response.status == 200 && token != null && contenu!="/") {
       const body1 = { id: idUser, token: token };
       fetch("/api/agenda/deleteAllEventClass", {
