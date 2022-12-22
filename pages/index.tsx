70142290f3e2a87cb2a5aae2328dc3f941ad5f19
Estelle JANS <e.jans@student.helmo.be>
2022-12-22 17:09:14 +0100

    cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -145,6 +145,7 @@ export default function Home() {
         var one = event.begin.toString();
         var pos = one.indexOf("\n");
         var oneEvent = one.substring(pos+2);
+        console.log(oneEvent);
         console.log( JSON.parse(oneEvent));
         var descriptionClass = event.description ?? "/";
         if (
