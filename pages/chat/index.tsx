7b7341ff7973274ca47afc3d6474d830cdce74fe
e180478 <a.wiander@student.helmo.be>
2022-12-22 22:09:25 +0100

    Try Catch

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -2,9 +2,11 @@ import ChatList from "../../components/ChatList";
 
 export default function Home() {
   return (
-    <div className="font-face-pg text-center">
-        <h1 className="text-2xl">Liste des discussions</h1>
-        <p className="text-md">Pour créer une discussion, il faut créer une requête de tutorat !</p>
+    <div className="font-face-pg text-center mt-5">
+      <h1 className="text-2xl">Liste des discussions</h1>
+      <p className="text-md">
+        Pour créer une discussion, il faut créer une requête de tutorat !
+      </p>
       <ChatList />
     </div>
   );
