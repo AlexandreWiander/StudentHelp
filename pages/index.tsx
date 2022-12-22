ba1e040201741101b086029b7eaf2ab7b707112a
Estelle JANS <e.jans@student.helmo.be>
2022-12-22 18:48:40 +0100

    cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -140,6 +140,7 @@ export default function Home() {
         var pos = one.indexOf("\\n");
         var oneEvent = one.substring(pos+2);
         let lines = oneEvent.split('\n');
+        console.log(lines);
         let obj = {} as any;
         for (let line of lines) {
           let parts = line.split(':');
