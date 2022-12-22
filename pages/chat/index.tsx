4e8e8be81ca39c5457a34237f0d1432fa83824b4
e180478 <a.wiander@student.helmo.be>
2022-12-22 22:21:11 +0100

    Try Catch

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -2,7 +2,7 @@ import ChatList from "../../components/ChatList";
 
 export default function Home() {
   return (
-    <div className="font-face-pg text-center mt-5">
+    <div className="font-face-pg text-center mt-10">
       <h1 className="text-2xl">Liste des discussions</h1>
       <p className="text-md">
         Pour créer une discussion, il faut créer une requête de tutorat !
