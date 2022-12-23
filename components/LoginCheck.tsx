ed8b1aff4372c5bdd46fbbac6864dc4acf6328af
e180478 <a.wiander@student.helmo.be>
2022-12-23 01:43:12 +0100

    Fix Chat

--- components/LoginCheck.tsx
+++ components/LoginCheck.tsx
@@ -59,16 +59,6 @@ export function LoginCheck({ children }: { children: JSX.Element }) {
             localStorage.clear();
             signOut();
             router.push("/connection");
-            toast.error("Vous êtes banni :)", {
-              position: "top-center",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "colored",
-            });
           }
         });
 
