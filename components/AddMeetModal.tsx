0598ce822805678b6893346dafd359e022ba3409
Estelle JANS <e.jans@student.helmo.be>
2022-12-23 15:31:28 +0100

    regex date + hour

--- components/AddMeetModal.tsx
+++ components/AddMeetModal.tsx
@@ -49,7 +49,7 @@ export default function DeleteUserModal({ id }: ModalProps, props: any) {
     var placeInput = document.getElementById("place") as HTMLInputElement;
     const validNom = new RegExp("^\\D{1,100}$");
     const validPlace = new RegExp("^.{1,100}$");
-    const validDate = new RegExp("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+    const validDate = new RegExp("^([0-2][0-9]|(3)[0-1])(-)(((0)[0-9])|((1)[0-2]))(-)\\d{4}$");
     const validHour = new RegExp("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$");
 
     if (
