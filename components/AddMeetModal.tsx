dd0caaebd38172bd5048d88357203a9500f16113
Estelle JANS <e.jans@student.helmo.be>
2022-12-23 15:21:09 +0100

    regex date + hour

--- components/AddMeetModal.tsx
+++ components/AddMeetModal.tsx
@@ -8,9 +8,6 @@ type ModalProps = {
 
 let token: string | null = "";
 let idUser = -1;
-let dateNow = new Date();
-let stringDate = dateNow.getFullYear()+"-"+(dateNow.getMonth()+1)+"-"+dateNow.getDate();
-let hourDate = dateNow.getHours()+":"+dateNow.getMinutes();
 
 export default function DeleteUserModal({ id }: ModalProps, props: any) {
   const [showModal, setShowModal] = React.useState(false);
@@ -229,7 +226,6 @@ export default function DeleteUserModal({ id }: ModalProps, props: any) {
                       <input
                         id="date"
                         max="2024-12-31"
-                        value={stringDate}
                         type="date"
                         className="form-control block w-full text-center px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="Select a date"
@@ -240,7 +236,6 @@ export default function DeleteUserModal({ id }: ModalProps, props: any) {
                       <input
                         id="start"
                         type="time"
-                        value={hourDate}
                         className="form-control block w-full text-center px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="Select a date"
                       />
@@ -250,7 +245,6 @@ export default function DeleteUserModal({ id }: ModalProps, props: any) {
                       <input
                         id="end"
                         type="time"
-                        value={hourDate}
                         className="form-control block w-full text-center py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="Select a date"
                       />
