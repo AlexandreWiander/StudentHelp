a1e416610c99b9fd6ef56500608e039a6443f439
Estelle JANS <e.jans@student.helmo.be>
2022-12-23 14:42:20 +0100

    validation champ contact (max 400 caractères)

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,6 +82,8 @@ export default function Home() {
   }, []);
 
   const deleteRequest = (event: { currentTarget: { id: any } }) => {
+    eventsArr.length=0;
+    setEvent(eventsArr);
     const body = {
       id: event.currentTarget.id,
       token: token,
@@ -329,9 +331,6 @@ export default function Home() {
                       <p>{one["lieu"]}</p>
                     </div>
                     <div className="col-start-6 flex items-center">
-                      <button id={one["id"]} onClick={deleteRequest}>
-                        <img src={"images/block.png"} className="w-20" />
-                      </button>
                       <Link
                         href={{
                           pathname: "/chat/message",
@@ -346,6 +345,9 @@ export default function Home() {
                           <p className="text-center">Contacter</p>
                         </button>
                       </Link>
+                      <button id={one["id"]} onClick={deleteRequest}>
+                        <img src={"images/block.png"} className="w-20" />
+                      </button>
                     </div>
                   </div>
                 );
